5f45cbdf636185f870580173b803d93b8326990d
likhith78 <his-email-used-on-github>
2026-02-19 22:40:37 +0530

    SAMPLE 1 ADDED

---
+++ samplee1.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+rainfall = np.array([12, 8, 15, 10, 5])
+
+total_rain = np.sum(rainfall)
+avg_rain = np.mean(rainfall)
+
+print("Rainfall data:", rainfall)
+print("Total rainfall:", total_rain)
+print("Average rainfall:", avg_rain)
